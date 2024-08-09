8ad770e1bc210d72d5ee44284e1211ab02f9c5ed
1337Herman228 <geragets@gmail.com>
2024-08-09 19:04:02 +0300

    first commit

---
+++ src/test/java/com/example/amazingpcbackend/AmazingPcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazingpcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingPcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
